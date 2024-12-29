5c97451e5be51f1271b69e1c9b46bbe5261dde79
Rob Johnson <johnson.rob.alan@gmail.com>
2024-12-28 21:17:13 -0800

    adjust for 0-1 intensity scale

--- ppm.py
+++ ppm.py
@@ -13,13 +13,15 @@ def output_ppm(filename: str, image: list[list[RGB]], max_intensity: int = 255) 
         f.write(f"{max_intensity}\n")
         for row in image:
             for pixel in row:
-                f.write(f"{pixel.r} {pixel.g} {pixel.b} ")
+                f.write(
+                    f"{int(pixel.r * max_intensity)} {int(pixel.g* max_intensity)} {int(pixel.b* max_intensity)} "
+                )
             f.write("\n")
 
 
 if __name__ == "__main__":
     image = [
-        [RGB(255, 0, 0), RGB(0, 255, 0), RGB(0, 0, 255)],
-        [RGB(255, 255, 255), RGB(0, 0, 0), RGB(140, 140, 140)],
+        [RGB(1.0, 0, 0), RGB(0, 1.0, 0), RGB(0, 0, 1.0)],
+        [RGB(1.0, 1.0, 1.0), RGB(0, 0, 0), RGB(0.5, 0.5, 0.5)],
     ]
     output_ppm("output.ppm", image)
